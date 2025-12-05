4fba13f2b6865119908989c8eb937606d0f92148
vm <vkataev@gmail.com>
2025-12-05 22:16:23 +0100

    ADD llama-server launcher

--- run.llama-cli.sh
+++ run.llama-cli.sh
@@ -1,7 +1,7 @@
 #model=../models/gemma-3-1b-it-Q4_K_M.gguf
 #model=../models/gemma-3-27b-it-Q4_K_M.gguf
 model=../models/gemma-3-12b-it-Q4_K_M.gguf
-./build/bin/llama-cli \
+./build/bin/llama-server \
 	--model $model \
 	--seed 123 \
 	--prio 2 \
@@ -15,18 +15,4 @@ model=../models/gemma-3-12b-it-Q4_K_M.gguf
 	--color \
 	--threads 8 \
   --n-gpu-layers 55
-
-
-
-#	--n-gpu-layers 55
-	#--threads 8 \
-	#--ctx-size 16384 \
-	#--ctx-size 65536 \
-# \
-#	--interactive \
-#	-r "<start_of_turn>user" \
-#	--file /Users/vm/Documents/proj/3rd_party/local/prompts/gemma3_demo.txt
-
-
-#check GPU: system_profiler SPDisplaysDataType
 
--- run.llama-server.sh
+++ run.llama-server.sh
@@ -1,1 +1,20 @@
-./build/bin/llama-server.exe -m ../models/gemma-3n-E4B-it-Q4_K_M.gguf --host 127.0.0.1 --port 8033
+#./build/bin/llama-server.exe -m ../models/gemma-3n-E4B-it-Q4_K_M.gguf --host 127.0.0.1 --port 8033
+#model=../models/gemma-3-1b-it-Q4_K_M.gguf
+#model=../models/gemma-3-27b-it-Q4_K_M.gguf
+#model=../models/gemma-3-12b-it-Q4_K_M.gguf
+model=../models/Ministral-3-14B-Instruct-2512-UD-Q4_K_XL.gguf
+./build/bin/llama-server \
+	--model $model \
+	--seed 123 \
+	--prio 2 \
+	--temp 0.7 \
+	--repeat-penalty 1.0 \
+	--min-p 0.01 \
+	--top-k 64 \
+	--top-p 0.95 \
+	--ctx-size 16384 \
+	--threads 16 \
+    --host 127.0.0.1 \
+    --port 3088 \
+    --n-gpu-layers 55
+
